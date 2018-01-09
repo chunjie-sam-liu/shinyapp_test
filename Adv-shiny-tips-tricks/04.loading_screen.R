94e4e8911bbb47cede82c36869cf5222c7a028e5
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-01-09 12:13:08 +0800

    loading screen

--- Adv-shiny-tips-tricks/04.loading_screen.R
+++ Adv-shiny-tips-tricks/04.loading_screen.R
@@ -44,3 +44,97 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+
+library(shiny)
+library(shinyjs)
+
+load_data <- function() {
+  Sys.sleep(2)
+  hide("loading_page")
+  show("main_content")
+}
+
+ui <- fluidPage(
+  useShinyjs(),
+  div(
+    id = "loading_page",
+    h1("Loading...")
+  ),
+  hidden(
+    div(
+      id = "main_content",
+      "Data loaded, content goes here"
+    )
+  )
+)
+
+server <- function(input, output, session) {load_data()}
+
+shinyApp(ui = ui, server = server)
+
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    actionButton(inputId = "btn_data", label = "Download"),
+    conditionalPanel(condition = "output.setupComplete",
+                     box( title = "box1" ),
+                     box( title = "box2" ),
+                     box( title = "boc3" )
+    ),
+    conditionalPanel(condition = "!output.setupComplete",
+                     box( title = "loading"))
+  )
+)
+
+server <- function(input, output) { 
+  
+  rv <- reactiveValues()
+  rv$setupComplete <- FALSE
+  
+  ## simulate data load
+  observe({
+    
+    if(input$btn_data){
+      
+      df <- data.frame(id = seq(1,200),
+                       val = rnorm(200, 0, 1))
+      
+      ## Simulate the data load
+      Sys.sleep(5)
+      ## set my condition to TRUE
+      rv$setupComplete <- TRUE
+    }
+    
+    ## the conditional panel reads this output
+    output$setupComplete <- reactive({
+      return(rv$setupComplete)
+    })
+    outputOptions(output, 'setupComplete', suspendWhenHidden=FALSE)
+    
+  })
+}
+
+shinyApp(ui, server)
+
+shinyUI(pageWithSidebar(
+  headerPanel("Action button example"),
+  sidebarPanel(actionButton("button1", "New samples")),
+  mainPanel(plotOutput("plot"))
+)) -> ui
+
+shinyServer(function(input, output) {
+  output$plot <- reactivePlot(function() {
+    input$button1
+    hist(rnorm(20))
+  })
+}) -> server
+
+shinyApp(ui = ui, server = server)
+
+
+
